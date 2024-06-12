1959383baf1ceb556d011585009859d6fd911c3b
Anastasiia <vasilisandreeva00@gmail.com>
2024-06-12 19:02:12 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# AnastasiiaHombalevska2D_portfolio_game-
-My resume as 2D game
+# AnastasiiaHombalevska-2D-portfolio-game
+
+You can test the game and familiarize yourself with cv : [DEMO LINK](https://anastasiiahombalevska.github.io/AnastasiiaHombalevska-2D-portfolio-game/)
+
+![A screenshot of the project](./developerportfolio.png)
---
+++ src/constants.js
@@ -0,0 +1,33 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my computer. Now I'm mostly learning React with TypeScript.
+  I've done a few assignments throughout my studies, including topics on creating ToDo apps, paginated web pages, and more.
+  My goal is to move towards becoming a Fullstack developer.
+  Here is my <a href="https://github.com/AnastasiiaHombalevska" target="_blank">Github</a>!`,
+
+  "cs-degree": `This is my diploma in psychology. Because working as a psychologist in the education system is a period of life in which there is something to remember and tell.`,
+
+  "sofa-table": `That's my sofa. This is a very functional object. It is a work area, for example, the text you are reading now was written here. A place to relax with reading The Hobbit. A place to procrastinate by counting dots on the ceiling, and so on, depending on the request&#128512`,
+
+  tv: `That's my TV. I've been watching tech youtubers, art channels and training channels from very different areas:
+  <a href="https://www.youtube.com/@JSLegendDev" target="_blank">JSLegendDev</a>,
+  It's not YouTube, but let's count it too <a href="https://www.themysteztvo.com.ua/" target="_blank">The Mysteztvo</a>,
+  <a href="https://www.youtube.com/@Guardinaction" target="_blank">(It's interesting, but it may not be clear)</a> and i think that's enough.`,
+
+  bed: `The best way to recover is to lie on the bed (on the floor is another great option&#128512).
+  This is also where I sleep, of course. Great ideas come to me in the middle of the night. When an idea comes to me, I often have to write it down, otherwise I won't be able to sleep.`,
+
+  resume: `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/1aRCgISG6vP2Rt-9pi7w3LoNnpqjo1g0mJg7ivTFqDzY/edit?usp=sharing" target="_blank">Check it out?</a>
+  I guess I need to add this game to it as well.
+  <a href="mailto:vasilisandreeva00@gmail.com" target="_blank">Contact me</a> if you have any interesting job opportunities!`,
+
+  projects: `Information about this portfolio : Created with Kaboom.js library, which is a library for creating games in JavaScript.
+  The text is rendered using HTML/CSS. Therefore, the text box you are reading now is not displayed on the canvas.
+  In general, this is an additional way to tell you that I am looking for my first job as a front-end developer`,
+
+  library: `There are a lot of books on my shelves, including programming books. But i have a question. Who else compulsively buys books without ever finishing them?<br>
+  Among the books, you can find notebooks that I create in my free time. <a href="https://www.instagram.com/vasinu_bloknotu?igsh=MXRpcjFzdXMwOTRpcA==" target="_blank">Check them out</a>`,
+
+  exit: `Do you want to leave? A kindly reminder: to contribute to Ukraine's victory - <a href="https://savelife.in.ua/donate/#donate-army-card-monthly" target="_blank">click here.</a> And if you want to exit this portfolio, just close the tab.`,
+};
